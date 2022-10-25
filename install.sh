ffe9b92ab240694645ec433cdd8a86218631672e
Morgan Denes <mdenes@projetech.com>
2022-10-25 15:17:56 +0000

    Try to fix VimPlug

--- install.sh
+++ install.sh
@@ -22,8 +22,9 @@ if ! [ -x "$(command -v nvim)" ]; then
         cd neovim || return
         git checkout stable
         cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	echo "dotfile: Adding VimPlug"
+	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
     echo "dotfile: Installing nvim"
     sudo make install
