83efea1d03c9cff1b1a1f21367bd6a686e939392
Morgan <mdenes@LT000067>
2023-07-24 10:23:26 -0400

    Disabled lspsaga

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,43 +1,55 @@
-
 return {
-    -- {'glepnir/lspsaga.nvim'},
+    {
+      'glepnir/lspsaga.nvim',
+      lazy = true,
+    },
     {
         'neovim/nvim-lspconfig',
         lazy = false,
         dependencies = {
             'williamboman/mason-lspconfig.nvim',
             'hrsh7th/cmp-nvim-lsp',
-            'glepnir/lspsaga.nvim',
+            -- 'glepnir/lspsaga.nvim',
         },
         config = function(_, _)
             local set = vim.keymap.set
+            local saga = false
             require('pixel/colorsigns').lsp()
             local on_attach = function(_, bufnr)
-                local bufopts = {noremap = true, silent = true, buffer = bufnr}
-                set('n', 'gd', vim.lsp.buf.definition, bufopts)
-                set('n', 'gh', vim.lsp.buf.hover, bufopts)
-                set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts)
-                set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
-                set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+              local bufopts = {noremap = true, silent = true, buffer = bufnr}
+              set('n', 'gd', vim.lsp.buf.definition, bufopts)
+              set('n', 'gh', vim.lsp.buf.hover, bufopts)
+              set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts)
+              set('n', '<leader>gr', vim.lsp.buf.references, bufopts)
+              set('i', '<C-Space>', vim.lsp.buf.completion, bufopts)
+              if saga then
                 set({'n', 'v'}, '<leader>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
-                set('i', '<C-Space>', vim.lsp.buf.completion, bufopts)
-                -- set('n', '<leader>rn', "<cmd>Lspsaga rename<CR>", bufopts)
+                set('n', '<leader>rn', "<cmd>Lspsaga rename<CR>", bufopts)
                 set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
                 set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
                 set("n", "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
                 set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
                 set("n","<leader>o", "<cmd>Lspsaga outline<CR>")
-                set({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+                set({"n", "t"}, "<A-x>", "<cmd>Lspsaga term_toggle<CR>")
+              else
+                set({'n', 'v'}, '<leader>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
+                set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+                -- set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+                -- set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+                -- set("n", "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>")
+                -- set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+                -- set("n","<leader>o", "<cmd>Lspsaga outline<CR>")
+                -- set({"n", "t"}, "<A-x>", "<cmd>Lspsaga term_toggle<CR>")
+              end
             end
 
-            require'lspsaga'.setup({
-                ui = {
-                    border = 'rounded'
-                }
-            })
+            -- require'lspsaga'.setup({
+            --     ui = {
+            --         border = 'rounded'
+            --     }
+            -- })
 
             local lsp = require('lspconfig')
-            local home = os.getenv("HOME")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
             -- LUA --
@@ -81,6 +93,21 @@ return {
             lsp.volar.setup{
               -- Take over mode
               filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+              on_attach = on_attach,
+              capabilities = capabilities,
+            }
+
+            -- English --
+            require'lspconfig'.ltex.setup{
+              ltex = { language = "en-US" },
+              filetypes = {
+                "gitcommit",
+                "markdown",
+                "plaintex",
+                "plaintext",
+                "text",
+                "vimwiki",
+              },
               on_attach = on_attach,
               capabilities = capabilities,
             }
