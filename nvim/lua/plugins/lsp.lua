1c79e1243854ba5f756025379f79d8b4bbeaeb1a
Morgan <morgan@LT000337>
2025-10-21 11:48:32 -0400

    Resolve lspconfig deprication warning

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
             --     }
             -- })
 
-            local lsp = require('lspconfig')
+            local lsp = vim.lsp.config
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
             -- LUA --
